d6b64614cd448be8598c1d26229011ab16272049
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-07-09 12:30:06 +0600

    first commit

---
+++ src/app/Component/AddTask.jsx
@@ -0,0 +1,15 @@
+
+import {AiOutlinePlus} from 'react-icons/Ai'
+const AddTask = () => {
+    return (
+        <div className="grid 
+         justify-items-center">
+         
+          <div className='flex btn bg-blue-400 text-black'>
+          <button className="">Add new task </button> <div className='my-auto'><AiOutlinePlus/></div>
+          </div>
+        </div>
+    );
+};
+
+export default AddTask;
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import AddTask from '../app/Component/AddTask'
+export default function Home() {
+  return (
+    <main >
+      <h1 className=" text-center"> Task Manager</h1>
+      <AddTask/>
+         </main>
+  )
+}
